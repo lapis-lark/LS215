855a88f3f9e71b7f80946a497e9e9210b7d9995c
lapis <lapis.santuri@gmail.com>
2023-11-27 18:03:02 -0800

    class records done git status

--- list_processing/classRecordsReDo.js
+++ list_processing/classRecordsReDo.js
@@ -55,6 +55,24 @@ let studentScores = {
   },
 };
 
+const EXAM_WEIGHT = .65;
+const EXERCISE_WEIGHT = .35;
+
+function generateStudentGrades(scores) {
+  let studentGrades = [];
+  let students = Object.entries(scores).map(student => student[1].scores);
+  students.forEach(student => {
+    studentGrades.push([average(student.exams), average(student.exercises)]);
+  })
+
+  studentGrades = studentGrades.map(student => {
+    let grade = (student[0] * EXAM_WEIGHT) + (student[1] * EXERCISE_WEIGHT * 5);
+    return grade.toFixed(1);
+  })
+
+  return studentGrades;
+}
+
 function groupScoresByTest(studentScores) {
   let groupedTests = [];
   for (let i = 0; i < studentScores[1].length; i += 1) {
@@ -86,10 +104,20 @@ function average(numArr) {
   return numArr.reduce((sum, ele) => { return sum + ele }, 0) / numArr.length;
 }
 
+function compareNums(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+
+  return 0;
+}
+
 function getExamStats(exams) {
   let stats = [];
   exams.forEach(exam => {
-    sorted = exam.sort()
+    let sorted = exam.sort(compareNums);
     let ave = Number(average(exam).toFixed(2));
     let maximum = sorted.slice(-1)[0];
     let minimum = sorted[0];
@@ -101,20 +129,22 @@ function getExamStats(exams) {
 }
 
 
+
 function generateClassRecordSummary(scores) {
   let scoreArrays = getScores(scores);
   let {exams, exercises} = scoreArrays;
 
   let groupedExams = groupScoresByTest(exams);
   let examStats = getExamStats(groupedExams);
+  let studentGrades = generateStudentGrades(scores);
 
-  return examStats;
+  return {studentGrades, examStats};
 }
 
-generateClassRecordSummary(studentScores);
+console.log(generateClassRecordSummary(studentScores));
 
 // returns:
-{
+({
   studentGrades: [ '87 (B)', '73 (D)', '84 (C)', '86 (B)', '56 (F)' ],
   exams: [
     { average: 75.6, minimum: 50, maximum: 100 },
@@ -122,4 +152,4 @@ generateClassRecordSummary(studentScores);
     { average: 87.6, minimum: 60, maximum: 100 },
     { average: 91.8, minimum: 80, maximum: 100 },
   ],
-}
+})
