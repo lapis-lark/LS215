9a4c158c46fe089a638bae46aaeac90ba8203f20
lapis <lapis.santuri@gmail.com>
2023-12-17 17:11:33 -0800

    rail cipher complete!

--- generalProblemSolving/railCipher.js
+++ generalProblemSolving/railCipher.js
@@ -1,17 +1,3 @@
-/*
-  PROBLEM: create methods to encode and decode rail ciphers
-        ENCODE METHOD:
-                        - print rails
-                        - print ciphered text
-        DATA STRUCT:
-                    array of arrays (each row as subarray)
-        
-        ALGO: 
-              have order array 
-
-
-*/
-
 function encodeRail(text) {
   const ORDER = [0,1,2,1];
 
@@ -30,5 +16,43 @@ function encodeRail(text) {
   console.log(rails.flat().join(''));
 }
 
-encodeRail('WE ARE DISCOVERED FLEE AT ONCE');
-encodeRail('I LIKE BANANAS SO WHAT');
+function decodeRail(text) {
+  let rowLength = Math.floor(text.length / 4);
+  remainder = text.length % 4;
+  let [row1, row2, row3] = [rowLength, rowLength * 2, rowLength];
+
+  switch(remainder) {
+    case 1:
+      row1 += 1;
+      break;
+    case 2: 
+      row1 += 1;
+      row2 += 1;
+      break;
+    case 3:
+      row1 += 1;
+      row2 += 1;
+      row3 += 1;
+      break;
+  }
+
+  row1text = text.slice(0, row1).split('');
+  row2text = text.slice(row1, row1 + row2).split('');
+  row3text = text.slice(row1 + row2).split('');
+
+  let deciphered = [];
+  while (row2text.length > 0) {
+    deciphered.push(row1text.shift());
+    deciphered.push(row2text.shift());
+    deciphered.push(row3text.shift());
+    deciphered.push(row2text.shift());
+  }
+
+  return deciphered.join('');
+}
+
+// encodeRail('ABCD');
+// encodeRail('ABCDEFGH');
+encodeRail('HELLO FRIENDS HOW ARE YOU MY HEARTIES')
+console.log(decodeRail('HOEHRUEIELFINSOAEOMHATELRDWYYRS'));
+
